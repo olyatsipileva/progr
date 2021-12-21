7edf53160bf2a5417866a8c07b5dbeb16bac2a9a
igor57690 <igor57690@yandex.ru>
2021-12-21 22:49:40 +0800

    готово

--- progr/Emitter.cs
+++ progr/Emitter.cs
@@ -10,14 +10,14 @@ namespace progr
         List<Particle> particles = new List<Particle>();
         
         public float GravitationX = 0;
-        public float GravitationY = 1; // пусть гравитация будет силой один пиксель за такт, нам хватит
+        public float GravitationY = 0.7f; // пусть гравитация будет силой один пиксель за такт, нам хватит
         public int X; // координата X центра эмиттера, будем ее использовать вместо MousePositionX
         public int Y; // соответствующая координата Y 
         public int Direction = 0; // вектор направления в градусах куда сыпет эмиттер
         public int Spreading = 360; // разброс частиц относительно Direction
         public float Speed = 0.05f;
-        public int SpeedMin = 1; // начальная минимальная скорость движения частицы
-        public int SpeedMax = 10; // начальная максимальная скорость движения частицы
+        public int SpeedMin = 10; // начальная минимальная скорость движения частицы
+        public int SpeedMax = 20; // начальная максимальная скорость движения частицы
         public int RadiusMin = 2; // минимальный радиус частицы
         public int RadiusMax = 10; // максимальный радиус частицы
         public int LifeMin = 20; // минимальное время жизни частицы
@@ -25,7 +25,7 @@ namespace progr
         public int ParticlesPerTick = 1;
         public Color ColorFrom = Color.White; // начальный цвет частицы
         public Color ColorTo = Color.FromArgb(0, Color.Magenta); // конечный цвет частиц
-        public int Radius = 60;
+        public int Radius = 80;
         public int ParticlesCount = 500;
         public float Time = 0;
         // добавил новый метод, виртуальным, чтобы переопределять можно было
@@ -105,30 +105,9 @@ namespace progr
                 particle.Draw(g, Radius, Time);
             }
             Time += Speed;
-            //cos(α) = (a * b) / (|a| * |b|)
-            float x1 = particles[^1].x - X;
-            float y1 = particles[^1].y - Y;
-            //float x2 = (Radius / 4) * Convert.ToSingle(Math.Cos(3));
-            //float y2 = (Radius / 4) * Convert.ToSingle(Math.Sin(3));
-            float x2 = 1;
-            float y2 = 1;
-            float direction = Convert.ToSingle((x1 * x2 + y1 * y2) / (Math.Sqrt(x1 * x1 + y1 * y1) * Math.Sqrt(x2 * x2 + y2 * y2)));
-            try
-            {
-                    if (Direction > 0)
-                    {
-                        if (Direction == 180)
-                        Direction = Convert.ToInt32(Math.Acos(direction) * 180 / Math.PI);
-                    }
-                if (Direction < 0)
-                    if 
-                    Direction = -Convert.ToInt32(Math.Acos(direction) * 180 / Math.PI);
-            }
-            catch(OverflowException)
-            {
-                
-            }
-            //Direction = -360;
+
+            Direction = -Convert.ToInt32(Time * 180 / Math.PI) + 90;
+
             g.DrawEllipse(
                new Pen(Color.White),
                X - Radius / 2,
